52924af6527968ca8b9be35890d90651ffbd5f03
root <ajimobikareemah@gmail.com>
2022-04-22 06:57:41 -0700

    fix documentation for task 0

--- _printf.c
+++ _printf.c
@@ -3,14 +3,8 @@
 
 /**
  * _printf - Function that prints formatted output.
- *
- * @format: a string composed of zero or more characters to print or use as
- * directives that handle subsequent arguments and special characters.
- *
- * Description: This function can take a variable number and type of arguments
- * that should be printed to standard output.
- *
- * Return: int
+ * @format: a string composed of zero or more characters.
+ * Return: characters printed
  */
 
 int _printf(const char *format, ...)
